fb32787e19d247dd46a0b02d07bfebfd9268c6bd
ana daniela <ramirez.hernandez.ana5im01@gmail.com>
2021-11-22 23:40:55 -0600

    socket.io

--- app.js
+++ app.js
@@ -40,21 +40,3 @@ app.listen(PORT,()=>{
     console.log('SERVER UP runnung in http://localhost:3000')
 })
 
-//ojo anaaaa
-
-var http = require('http');
-var server = http.Server(app);
-
-app.use(express.static('client'));
-
-server.listen(PORT, function() {
-  console.log('Chat server running');
-});
-
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
-});
